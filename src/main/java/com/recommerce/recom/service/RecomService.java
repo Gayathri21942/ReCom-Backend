0a703b1eaf363b76bd69ce1c660c24a580d5c25e
AkshaySubbaram <subbaramakshay@gmail.com>
2026-05-30 00:23:51 +0530

    Initial commit

--- src/main/java/com/recommerce/recom/service/RecomService.java
+++ src/main/java/com/recommerce/recom/service/RecomService.java
@@ -8,6 +8,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Service
 public class RecomService {
 
@@ -20,9 +23,52 @@ public class RecomService {
     }
 
     // SIGNUP
+//    public String signup(SignupRequest request) {
+//
+//        System.out.println("Raw password: " + request.password);
+//
+//        if (recomRepo.findByUsername(request.username).isPresent()) {
+//            throw new RuntimeException("Username already exists");
+//        }
+//
+//        if (recomRepo.findByEmail(request.email).isPresent()) {
+//            throw new RuntimeException("Email already exists");
+//        }
+//
+//        RecomEntity user = new RecomEntity();
+//        user.setUsername(request.username);
+//        user.setEmail(request.email);
+//        user.setPassword(passwordEncoder.encode(request.password));
+//
+//        recomRepo.save(user);
+//
+//        return "User registered successfully";
+//    }
+
     public String signup(SignupRequest request) {
 
         System.out.println("Raw password: " + request.password);
+
+// Allowed email domains
+        List<String> allowedDomains = Arrays.asList(
+                "gmail.com",
+                "yahoo.com",
+                "outlook.com",
+                "hotmail.com"
+        );
+
+// Check email format
+        if (request.email == null || !request.email.contains("@")) {
+            throw new RuntimeException("Invalid email format");
+        }
+
+// Extract domain
+        String domain = request.email.substring(request.email.indexOf("@") + 1);
+
+// Validate domain
+        if (!allowedDomains.contains(domain.toLowerCase())) {
+            throw new RuntimeException("Only valid email domains are allowed");
+        }
 
         if (recomRepo.findByUsername(request.username).isPresent()) {
             throw new RuntimeException("Username already exists");
@@ -40,7 +86,9 @@ public class RecomService {
         recomRepo.save(user);
 
         return "User registered successfully";
+
     }
+
 
     // LOGIN
     public String login(LoginRequest request) {
